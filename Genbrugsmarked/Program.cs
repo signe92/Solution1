e222feb80e6378a3aa13cb772a9dfc0fa6c91d00
Frederikmou <eaa24fmou@students.eaaa.dk>
2025-11-18 08:42:54 +0100

    Login Page
    Login Page næsten færdig

--- Genbrugsmarked/Program.cs
+++ Genbrugsmarked/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Genbrugsmarked;
+using Blazored.LocalStorage;
+using Genbrugsmarked.Service;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<BrugerRepo>();
+
+// brug af local storage
+builder.Services.AddBlazoredLocalStorage();
+
 
 await builder.Build().RunAsync();

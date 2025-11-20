98a602824bff4fa4072351bcb756d93d221a80d0
MagnusGregers <eaa24mgmw@students.eaaa.dk>
2025-11-20 09:48:15 +0100

    Fikset mongoDb

--- Core/Modeller/Annonce.cs
+++ Core/Modeller/Annonce.cs
@@ -1,13 +1,19 @@
-﻿namespace Core.Klasser;
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Core.Klasser;
 
 public class Annonce
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     
-    public int _id { get; set; }
+    public ObjectId _id { get; set; }
     public int AnnonceID { get; set; }
     public string Titel  { get; set; }
     public string Beskrivelse { get; set; }
     public double Pris { get; set; }
     public bool Status { get; set; }
-    public DateTime OprettetDato { get; set; }
+    public DateTime OprettetDato { get; set; } = DateTime.Now;
 }
